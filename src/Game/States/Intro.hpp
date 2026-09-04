ea0fa4ba74bc2e7caa5d596fbaefcc9dd4bd72fc
LilAkai_ <wolphi3035@gmail.com>
2026-09-04 14:50:45 +0200

    update to SFML 3.1.0 on MacOS

---
+++ src/Game/States/Intro.hpp
@@ -0,0 +1,12 @@
+#include <Engine/Core/StateMachine/State.hpp>
+#include <Engine/Core/StateMachine/StateManager.hpp>
+
+#include <SFML/Graphics/
+
+class IntroState final: public Core::State {
+public:
+
+
+private:
+    sf::RectangleShape
+};
